ed4edec8b44f367e353d8b66c58d48050af3caff
Albino Junior <albinojuniorv@gmail.com>
2019-03-11 18:37:22 -0300

    update api routes config

--- src/app/routes/api.ts
+++ src/app/routes/api.ts
@@ -1,15 +1,27 @@
-import UserRouter from "../modules/user/user.router";
 import { Router } from "express";
+
+import { plural } from "pluralize";
+import { getDirectories } from "../utils/functions.utils";
+import { resolve } from "path";
+import { existsSync } from "fs";
 
 export class ApiRouter {
   router: Router = Router();
+  exceptRoutes: string[] = ["auth"];
 
   constructor() {
     this.init();
   }
 
   init(): void {
-    this.router.use('/users', UserRouter);
+    let modules = getDirectories(resolve("src/app/modules"));
+    modules = modules.filter(module => this.exceptRoutes.indexOf(module) < 0);
+
+    modules.forEach(module => {
+      if (existsSync(resolve(`src/app/modules/${module}/${module}.router.ts`))) {
+        this.router.use(`/${plural(module)}`, require(`../modules/${module}/${module}.router`).default)
+      }
+    });
   }
 
 }
--- src/app/utils/functions.utils.ts
+++ src/app/utils/functions.utils.ts
@@ -1,5 +1,12 @@
+import { lstatSync, readdirSync } from "fs";
+import { join } from "path";
+
 import { NUMBER_TYPES } from '../common/Constraints';
 
-export function isNumberType(type: any): boolean {
-  return NUMBER_TYPES.filter(numberType => type instanceof numberType).length > 0;
-}
+export const isNumberType = (type: any): boolean => NUMBER_TYPES.filter(numberType => type instanceof numberType).length > 0;
+
+export const removeEspecialChars = (value: string): string => value.replace(/\D/gi, "");
+
+export const isDirectory = (source: string) => lstatSync(source).isDirectory();
+
+export const getDirectories = (source: string): string[] => readdirSync(source).filter(name => isDirectory(join(source, name)));
